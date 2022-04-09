0f60bfc1b9fafc2f1ada049c620fa1a3bed72941
jicu mollick <jikumollick82@gmail.com>
2022-04-09 15:14:20 +0600

    added conditional header shown

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
 
 const Login = () => {
+  const { signInWithGoogle } = useFirebase();
+
+  // console.log(signInwithGoogle);
   return (
     <div>
       <h2>Please Login</h2>
+      <div style={{ margin: "20px" }}>
+        <button onClick={signInWithGoogle}>Google Sign In</button>
+      </div>
+      <form action="">
+        <input type="email" placeholder="Your Email" name="" id="" /> <br />
+        <input type="password" placeholder="Your Password" /> <br />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 };
